895ca2c58aeb91e35f3e2af8ad6817fd1893f33e
rodriguezmauricio <contato.mauricioalves@gmail.com>
2024-04-30 19:54:43 +0100

    created the tables in the database. They're working

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,11 @@
+// import { NextAuthOptions } from "next-auth";
+// import { CredentialsProvider } from "next-auth/providers/credentials";
+// import Google from "next-auth/providers/google";
+
+// // import prisma from "./prisma"
+
+// export const authConfig: NextAuthOptions = {
+//   providers: [
+//     Credential
+//   ]
+// }
